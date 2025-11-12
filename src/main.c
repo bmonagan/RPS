93296c971f6ae4d3176c6b7fa84bf1ecef320d5d
bmonagan <bernardmonagan@gmail.com>
2025-11-11 22:17:03 -0500

    added computer_choice

--- src/main.c
+++ src/main.c
@@ -4,9 +4,13 @@ enum Choices { Rock, Paper, Scissors };
 int valid_choice(char* choice) {
     return (strcmp(choice, "Rock") == 0 || strcmp(choice, "Paper") == 0 || strcmp(choice, "Scissors") == 0);
 }
+int computer_choice() {
+    return rand() % 3; // Random choice between 0 (Rock), 1 (Paper), 2 (Scissors)
+}
 int main(){
     char* user_choice[10];    
     printf("Enter your choice: ");
+    // Waits for valid input from user.
     while (1) {
         scanf("%*s", user_choice);
         if (!valid_choice(*user_choice)) {
